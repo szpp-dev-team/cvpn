188a7119a2d73b8d61c92bbd9ceb8736ce1cd646
earlgray <earlgray283@gmail.com>
2021-03-07 15:31:32 +0900

    feat: ログインだけ

--- cmd/cvpn/main.go
+++ cmd/cvpn/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Shizuoka-Univ-dev/cvpn/api"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	username := os.Getenv("SVPN_USERNAME")
+	password := os.Getenv("SVPN_PASSWORD")
+
+	fmt.Println(username, password)
+
+	client := api.NewClient()
+	if err := client.Login(username, password); err != nil {
+		log.Fatal(err)
+	}
 }
