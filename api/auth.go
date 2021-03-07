ae44a2155389a98134839774ca43274f7252488c
earlgray <earlgray283@gmail.com>
2021-03-07 15:31:14 +0900

    feat: auth api

--- api/auth.go
+++ api/auth.go
@@ -1,3 +1,91 @@
 // login とか logout とか
 
 package api
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+func (self *Client) Login(username string, password string) error {
+	const (
+		LoginPayload = "https://vpn.inf.shizuoka.ac.jp/dana-na/auth/url_3/login.cgi"
+		LoginFailed  = "/dana-na/auth/url_3/welcome.cgi?p=failed"
+		LoginSucceed = "/dana/home/index.cgi"
+	)
+
+	parms := map[string][]string{
+		"tz_offset": {"540"},
+		"username":  {username},
+		"password":  {password},
+		"realm":     {"Student-Realm"},
+		"btnSubmit": {"Sign+In"},
+	}
+
+	resp, err := self.client.PostForm(LoginPayload, parms)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusFound {
+		return errors.New("Error: Please logout and login")
+	}
+
+	location := resp.Header.Get("location")
+	switch location {
+	case LoginSucceed:
+		// Header は map[string][]string の拡張
+		self.cookies = getCookies(resp.Header["Set-Cookie"])
+	case LoginFailed:
+		return errors.New("Error: Login Failed")
+	default: // confirm session
+		return errors.New("Oops! You should choose session(todo)")
+	}
+
+	return nil
+}
+
+func (self *Client) Logout() error {
+	const LogoutPayload = "https://vpn.inf.shizuoka.ac.jp/dana-na/auth/logout.cgi"
+
+	req, err := http.NewRequest(http.MethodGet, LogoutPayload, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := self.request(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusFound {
+		return errors.New("failed to logout")
+	}
+
+	self.cookies = getCookies(resp.Header["Set-Cookie"])
+	location := resp.Header.Get("location")
+
+	req, err = http.NewRequest(http.MethodGet, VpnHostRoot+location, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err = self.request(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("failed to logout")
+	}
+	
+	return nil
+}
+
+func getCookies(cookies []string) []string {
+	var parsedCookies []string
+	for _, line := range cookies {
+		tokens := strings.Split(line, "; ")
+		parsedCookies = append(parsedCookies, tokens[0])
+	}
+
+	return parsedCookies
+}
--- api/common.go
+++ api/common.go
@@ -1,3 +1,38 @@
 // 共通の api(cookie をつけたリクエストとか)
 
 package api
+
+import (
+	"net/http"
+	"strings"
+)
+
+const VpnHostRoot = "https://vpn.inf.shizuoka.ac.jp"
+
+type Client struct {
+	client  *http.Client
+	cookies []string
+}
+
+func NewClient() *Client {
+	client := new(http.Client)
+
+	// redirect をしない
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	return &Client{
+		client: client,
+	}
+}
+
+// need cookies
+func (self *Client) request(r *http.Request) (*http.Response, error) {
+	r.Header = map[string][]string{
+		"cookie": {strings.Join(self.cookies, "; ")}}
+
+	resp, err := self.client.Do(r)
+
+	return resp, err
+}
