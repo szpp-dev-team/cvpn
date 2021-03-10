37b93be89d7d36955277db83f7d667638e7e6891
earlgray <earlgray283@gmail.com>
2021-03-11 03:13:33 +0900

    feat: upload.go(empty)

---
+++ api/upload.go
@@ -0,0 +1,9 @@
+package api
+
+// dst は report をルートとしたパス
+func (c *Client) UploadFile(src, dst string) {
+	const UploadEndpoint = "https://vpn.inf.shizuoka.ac.jp/dana/fb/smb/wu.cgi"
+
+}
+
+// <input id="xsauth_372" type="hidden" name="xsauth" value="fc33f76bdcb8de99fe3c20d32b6cd71a">
