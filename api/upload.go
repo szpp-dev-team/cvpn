a3bd426cd77172716bade6aed3fe03160008cc9e
arumakan1727 <aluminumCan7271@gmail.com>
2021-03-15 01:10:52 +0900

    feat: ファイルアップロード関数を実装

--- api/upload.go
+++ api/upload.go
@@ -2,133 +2,149 @@ package api
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
-	"path/filepath"
+	"path"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const UploadEndpoint = "https://vpn.inf.shizuoka.ac.jp/dana/fb/smb/wu.cgi"
-
-// dst は report をルートとしたパス
-func (c *Client) UploadFile(src, dst string) error {
-	doc, err := c.getDoc(UploadEndpoint, nil)
-	if err != nil {
-		return err
-	}
-
-	trackid, err := findTrackid(doc)
-	if err != nil {
-		return err
-	}
-
-	return c.uploadFile(src, dst, trackid)
+type uploadReqInfo struct {
+	SrcPath       string
+	FileRenameOpt string // アップロード後に名前を変えるかどうか。変えないなら空文字列。
+	VolumeID      string
+	destDirPath   string
+	trackID       string
+	XSAuth        string
 }
 
-func (c *Client) uploadFile(src, dst, trackid string) error {
-	contentType, body, err := createFormBody(src)
+// ローカルの `srcPath` を `volumeID` ドライブの `destDirPath` へアップロードする。
+// `fileRenameOpt` が空文字列でなければ、アップロードしたファイルの名前は `fileRenameOpt` へ変更される。
+// `destDirPath` の '/' は '\' に置換されて送信される。
+func (c *Client) UploadFile(srcPath, fileRenameOpt, volumeID, destDirPath string) error {
+	destDirPath = strings.Replace(destDirPath, "/", "\\", -1)
+
+	trackID, err := c.scrapeTrackID4Upload(volumeID, destDirPath)
 	if err != nil {
 		return err
 	}
 
-	req, err := http.NewRequest(http.MethodPost, UploadEndpoint, body)
+	contentType, multipartBody, err := createMultipartBody4Upload(&uploadReqInfo{
+		SrcPath:       srcPath,
+		FileRenameOpt: fileRenameOpt,
+		VolumeID:      volumeID,
+		destDirPath:   destDirPath,
+		trackID:       trackID,
+		XSAuth:        c.authParams.Get("xsauth"),
+	})
+	if err != nil {
+		return err
+	}
+
+	// アップロード先URL には trackID の末尾にUNIX時間を追加したクエリパラメータを付与する
+	uploadURL := ("https://vpn.inf.shizuoka.ac.jp/dana/fb/smb/wu.cgi" +
+		"?trackid=" + trackID + strconv.FormatInt(time.Now().Unix(), 10))
+	req, err := http.NewRequest(http.MethodPost, uploadURL, multipartBody)
 	if err != nil {
 		return err
 	}
 	req.Header.Set("Content-Type", contentType)
-	req.Form = map[string][]string{
-		"dir":     {url.QueryEscape(strings.Replace("report"+dst, "/", "\\", -1))},
-		"acttype": {"upload"},
-		"confirm": {"yes"},
-		"trackid": {trackid},
-	}
 
 	resp, err := c.request(req)
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("Error: statuscode was not OK but %d", resp.StatusCode)
+		return fmt.Errorf("StatusCode of file uploading was %d (expected: 200 OK)", resp.StatusCode)
 	}
-
-	/*
-		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println(string(b))
-	*/
-
 	return nil
 }
 
-// https://medium.com/eureka-engineering/multipart-file-upload-in-golang-c4a8eb15a3ee
-// return {Content-Type}, body, error
-func createFormBody(path string) (string, *bytes.Buffer, error) {
-	fp, err := os.Open(path)
+func (c *Client) scrapeTrackID4Upload(volumeID, destDirPath string) (string, error) {
+	const uploadFormURL = "https://vpn.inf.shizuoka.ac.jp/dana/fb/smb/wu.cgi"
+
+	xsauth := c.authParams.Get("xsauth")
+	if len(xsauth) == 0 {
+		return "", fmt.Errorf("Client has no `xsauth`")
+	}
+	postForm := genCommonAccessParam(volumeID, destDirPath)
+	postForm.Set("acttype", "upload")
+	postForm.Set("ignoreDfs", "1")
+	postForm.Set("xsauth", xsauth)
+
+	req, err := http.NewRequest(http.MethodPost, uploadFormURL, strings.NewReader(postForm.Encode()))
 	if err != nil {
-		return "", nil, err
+		return "", err
 	}
-	defer fp.Close()
-
-	body := &bytes.Buffer{}
-	w := multipart.NewWriter(body)
-
-	filename := filepath.Base(path)
-	fw, err := w.CreateFormFile("file1", filename)
+	resp, err := c.request(req)
 	if err != nil {
-		return "", nil, err
+		return "", err
 	}
-
-	if _, err := io.Copy(fw, fp); err != nil {
-		return "", nil, err
+	defer resp.Body.Close()
+	docScraper, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return "", err
 	}
-
-	contentType := w.FormDataContentType()
-
-	return contentType, body, w.Close()
+	trackid, exists := docScraper.Find("#trackid_1").Attr("value")
+	if !exists {
+		return "", fmt.Errorf("`trackid` not found: request URL: %q", uploadFormURL)
+	}
+	return trackid, nil
 }
 
-func findTrackid(doc *goquery.Document) (string, error) {
-	selection := doc.Find(`
-		html > 
-		body > 
-		table#table_useruimenu_10.tdContent >
-		tbody >
-		tr >
-		td
-	`)
-	selection = selection.Next()
-	selection = selection.Find(`
-		form#frmUpload_4 >
-		table#table_wu_1.tdContent >
-		tbody >
-		tr >
-		td >
-		table#table_wu_3 >
-		tbody >
-		tr >
-		td >
-		table#table_wu_19 >
-		tbody >
-		tr >
-		input#trackid_1
-	`)
-	val, exists := selection.Attr("value")
-	if !exists {
-		return "", errors.New("Error: trackid not found")
+// https://medium.com/eureka-engineering/multipart-file-upload-in-golang-c4a8eb15a3ee
+func createMultipartBody4Upload(info *uploadReqInfo) (contentType string, body *bytes.Buffer, err error) {
+	formBodyBuff := &bytes.Buffer{}
+	mw := multipart.NewWriter(formBodyBuff)
+
+	// ファイルの書き出し
+	{
+		srcFile, err := os.Open(info.SrcPath)
+		if err != nil {
+			return "", nil, err
+		}
+		defer srcFile.Close()
+
+		partWriter, _ := mw.CreateFormFile("file1", path.Base(info.SrcPath))
+		if _, err = io.Copy(partWriter, srcFile); err != nil {
+			return "", nil, err
+		}
 	}
 
-	fmt.Println("trackID: " + val)
+	// フォームの普通のフィールドの書き出し
+	{
+		formFields := genCommonAccessParam(info.VolumeID, info.destDirPath)
+		formFields.Set("acttype", "upload")
+		formFields.Set("confirm", "yes")
+		formFields.Set("ignoreDfs", "1")
+		formFields.Set("trackid", info.trackID)
+		formFields.Set("txtRenameFile1", info.FileRenameOpt)
+		formFields.Set("txtServerUploadID", "")
+		formFields.Set("xsauth", info.XSAuth)
 
-	return val, nil
+		for key, values := range *formFields {
+			partWriter, err := mw.CreateFormField(key)
+			if err != nil {
+				return "", nil, err
+			}
+			if len(values) > 1 {
+				return "", nil, fmt.Errorf("The form value of %q cannot be multiple (got: %v)", key, values)
+			}
+			value := url.QueryEscape(formFields.Get(key))
+			if _, err = partWriter.Write([]byte(value)); err != nil {
+				return "", nil, err
+			}
+		}
+	}
+	if err := mw.Close(); err != nil {
+		return "", nil, err
+	}
+	return mw.FormDataContentType(), formBodyBuff, nil
 }
