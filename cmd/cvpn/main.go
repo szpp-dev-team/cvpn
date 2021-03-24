cf1b2d439657b1b9164b7b676503f51974097e8b
earlgray <earlgray283@gmail.com>
2021-03-24 20:32:36 +0900

    fix: command entry point

--- cmd/cvpn/main.go
+++ cmd/cvpn/main.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/Shizuoka-Univ-dev/cvpn/api"
-	"github.com/joho/godotenv"
+	"github.com/Shizuoka-Univ-dev/cvpn/pkg/subcmd"
 )
 
 func main() {
-
-	if err := godotenv.Load(); err != nil {
-		log.Fatal(err)
-	}
-
-	username := os.Getenv("SVPN_USERNAME")
-	password := os.Getenv("SVPN_PASSWORD")
-
-	fmt.Println(username, password)
-
-	client := api.NewClient()
-	if err := client.LoadCookiesOrLogin(username, password); err != nil {
-		log.Fatal(err)
-	}
-	if _, err := client.List(""); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("All success. exit 0.")
+	subcmd.Execute()
 }
