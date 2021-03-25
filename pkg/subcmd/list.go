72a5c7b62d7c1854c78d94579baf7bcba3cb4f73
KAJI <kajiharalivi@icloud.com>
2021-03-25 15:41:15 +0900

    文字列表示の調整中

--- pkg/subcmd/list.go
+++ pkg/subcmd/list.go
@@ -1,8 +1,8 @@
 package subcmd
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/Shizuoka-Univ-dev/cvpn/api"
@@ -17,14 +17,9 @@ func NewListCmd() *cobra.Command {
 		Aliases: []string{"ls"},
 		Short:   "list files and directorys from path",
 		Long:    "クソナガ説明",
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) != 1 {
-				_ = cmd.Help()
-				os.Exit(1)
-			}
-			// ls 処理
+		RunE: func(cmd *cobra.Command, args []string) error {
 			if err := godotenv.Load(); err != nil {
-				log.Fatal(err)
+				return err
 			}
 
 			username := os.Getenv("SVPN_USERNAME")
@@ -34,24 +29,78 @@ func NewListCmd() *cobra.Command {
 
 			client := api.NewClient()
 			if err := client.LoadCookiesOrLogin(username, password); err != nil {
-				log.Fatal(err)
+				return err
 			}
 
 			segInfos, err := client.List(args[0])
 			if err != nil {
-				log.Fatal(err)
+				return err
 			}
 
 			s := ""
 
-			fmt.Printf("index %4s |name %115s |size %4s |upload-at %4s\n", s, s, s, s)
+			/*hoge := func(n int) string {
+				if n%2 == 0 {
+					return "even"
+				} else {
+					return "odd"
+				}
+			}(4)*/
+			format := genFormat(segInfos)
+
+			fmt.Printf("index %4s name %50s size %4s upload-at %4s\n", s, s, s, s)
 			fmt.Println("ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー")
 
-			for i := 0; i < len(segInfos); i++ {
-				fmt.Printf("%-11d|%-121s|%f\n", i+1, segInfos[i].Name, segInfos[i].Size)
+			for i, _ := range segInfos {
+				//fmt.Println(segInfos[i].Unit)
+				//length := countStrLen(segInfos[i].Name)
+				if segInfos[i].Size == -1 {
+					fmt.Printf(format, "\n", i+1, segInfos[i].Name, segInfos[i].Path, s, segInfos[i].UpdatedAt)
+				} else {
+					f := fmt.Sprintf("%.2f", segInfos[i].Size)
+					fmt.Printf(format, "\n", i+1, segInfos[i].Name, f, segInfos[i].Unit, segInfos[i].UpdatedAt)
+				}
 			}
+
+			return nil
+		},
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("write the path to place that you wnat to see all files and folders")
+			}
+
+			return nil
 		},
 	}
-
 	return cmd
+}
+
+func genFormat(s []api.SegmentInfo) string {
+	format := `%%-%dd%%-%ds%%-%ds %%s %%s`
+	maxname := 0
+	maxpath := 0
+	for i, _ := range s {
+		if x1 := countStrLen(s[i].Name); maxname < x1 {
+			maxname = x1
+		}
+		if x2 := countStrLen(s[i].Path); maxpath < x2 {
+			maxpath = x2
+		}
+	}
+	a := fmt.Sprintf(format, 11, maxname, maxpath)
+	fmt.Println(a)
+	return a
+}
+
+func countStrLen(s string) int {
+	length := 0
+
+	for _, c := range s {
+		if '0' <= c && c <= '9' || 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' {
+			length += 1
+		} else {
+			length += 2
+		}
+	}
+	return length
 }
