a12d7b8900f49710425a32cf9cc01188f46ed411
earlgray <earlgray283@gmail.com>
2021-03-25 23:14:18 +0900

    style: format

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,43 +1,43 @@
 package config
 
 import (
-	"os"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"os"
 	"path"
 )
 
 //JSON用の構造体
 type Config struct {
-	Username  string `json:"username"`
-	Password  string `json:"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 //Username,passwordを返す（Config型）
-func LoadConfig() (Config,error) {
+func LoadConfig() (Config, error) {
 
 	//ConfigDirPathを取得
-	configDir , err := os.UserConfigDir()
+	configDir, err := os.UserConfigDir()
 	if err != nil {
-		return Config{},err
+		return Config{}, err
 	}
 
 	// ConfigFilePath（configFileを書き込むパス）の設定
-	configFilePath := path.Join(configDir,"cvpn/config.json") 
+	configFilePath := path.Join(configDir, "cvpn/config.json")
 
-	jsonFile,err := ioutil.ReadFile(configFilePath)
+	jsonFile, err := ioutil.ReadFile(configFilePath)
 
 	if err != nil {
-        return Config{},err
-    }
+		return Config{}, err
+	}
 
 	//Config型のデータ
-    var data Config
-    err = json.Unmarshal(jsonFile, &data)
-    if err != nil {
-        return Config{},err
-    }
+	var data Config
+	err = json.Unmarshal(jsonFile, &data)
+	if err != nil {
+		return Config{}, err
+	}
 
 	//username,password
-	return data,nil
+	return data, nil
 }
